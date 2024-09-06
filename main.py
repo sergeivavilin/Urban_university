3760023b4ead32ead72d63fe057e49af824e5699
Sergey Vavilin <vavilin.dev@gmail.com>
2024-09-06 18:52:55 +0300

    1-st task done

---
+++ main.py
@@ -0,0 +1,18 @@
+# "1st program"
+print(9 ** 0.5 * 5)
+
+# "2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# "3rd program"
+first_number = 2 * 2 + 2
+second_number = 2 * (2 + 2)
+
+print(first_number)
+print(second_number)
+print(first_number == second_number)
+
+# "4th program"
+exemple_string = '123.456'
+
+print(int(float(exemple_string) * 10) % 10)

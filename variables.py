ddcf41de6e0e077d81c776a369478673a3311580
Sergey Vavilin <vavilin.dev@gmail.com>
2024-09-07 15:33:03 +0300

    task variables done

---
+++ variables.py
@@ -0,0 +1,13 @@
+# Практическое задание по работе в Pycharm - "Переменные"
+
+completed_tasks = 12
+spent_hours = 1.5
+course_name = "Python"
+average_task_time = spent_hours / completed_tasks
+
+print(
+    f"Курс: {course_name},\n"
+    f"Всего задач: {completed_tasks},\n"
+    f"Затрачено часов: {spent_hours},\n"
+    f"Среднее время выполнение {average_task_time} часов"
+)

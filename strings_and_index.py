c6a24785e2da328ae6ff9380c08c9761741e1079
Sergey Vavilin <vavilin.dev@gmail.com>
2024-09-07 14:44:22 +0300

    task strings and indexes done

---
+++ strings_and_index.py
@@ -0,0 +1,9 @@
+# Практическое задание по уроку "Строки и индексация строк"
+
+exemple = "Топинамбур"
+
+print(exemple[0])  # Т
+print(exemple[-1])  # р
+print(exemple[5:])  # амбур
+print(exemple[-1::-1])  # рубманипоТ
+print(exemple[1::2])  # оиабр

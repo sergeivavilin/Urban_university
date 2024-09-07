8478b39413ffa9870349ca8ee3933d058c747e87
Sergey Vavilin <vavilin.dev@gmail.com>
2024-09-07 16:20:17 +0300

    task dynamic typing done

---
+++ module_1_3_(dynamic_typing).py
@@ -0,0 +1,12 @@
+# Практическая работа по уроку "Динамическая типизация"
+
+name = "Sergei"
+age = 37
+
+print(f"Name: {name}\nAge: {age}")
+
+age += 1
+print(f"New age: {age}")
+
+is_student = True
+print(f"Is Student: {is_student}")

b38d2b149b7a10cc8514b38fe871b3a89d8ab16c
Sergei <vavilin.dev@gmail.com>
2024-09-16 15:15:21 +0300

    task module_4_1 complete

--- Module_04/module_4_1/fake_math.py
+++ Module_04/module_4_1/fake_math.py
@@ -1,8 +1,6 @@
 def divide(first, second):
     try:
         result = first / second
-        return result
     except ZeroDivisionError:
-        return "Ошибка"
-
-
+        result = "Ошибка"
+    return result
